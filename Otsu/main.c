edb4ffd0b0353e18ca44648dc90934795a66be4c
Sankarsan Seal <sankarsanseal@icloud.com>
2017-03-04 07:51:45 +0530

    Initial Commit

---
+++ Otsu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Otsu
+//
+//  Created by Sankarsan Seal on 04/03/17.
+//  Copyright © 2017 Sankarsan Seal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
